5c3a320f8af8b0fc58bc08c4fc8e4b9251b63ce6
Joelle  Johnson <joellej221@gmail.com>
2023-11-07 12:53:44 -0500

    centered punchline

--- styles.js
+++ styles.js
@@ -35,8 +35,7 @@ const styles = StyleSheet.create({
     punchlineText: {
       color: '#00d',
       fontSize: 30,
-      justifyContent: 'center',
-      alignItems: 'center',
+      textAlign:'center',
     },
   })
 
