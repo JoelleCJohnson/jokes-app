8f37f9d08521e349e48c4d40e533c4385ec3d070
Joelle  Johnson <joellej221@gmail.com>
2023-11-07 12:52:03 -0500

    shuffle jokes

--- components/JokeList.jsx
+++ components/JokeList.jsx
@@ -3,38 +3,31 @@ import { Button } from 'react-native'
 import Joke from './Joke'
 import Punchline from './PunchLine'
 
-export default function JokeList(){
+export default function JokeList() {
     const [jokes, setJokes] = useState()
-    const[currentJoke, setCurrentJoke] = useState(0)
+    const [currentJoke, setCurrentJoke] = useState(0)
 
     useEffect(() => {
         fetch('https://api.sampleapis.com/jokes/goodJokes')
             .then(res => res.json())
-            .then(shuffleJokes)
+            .then(setJokes)
             .catch(alert)
+            .finally(shuffleJokes)
     }, [])
 
-    const nextJoke = () => {
-        if (currentJoke < jokes.length -1){
-            setCurrentJoke(currentJoke + 1)
-        } else {
-            setCurrentJoke(0) //loop back to first joke
+    const shuffleJokes = () => {
+        if(jokes){
+
+            const j = Math.floor(Math.random() * jokes.length)//(i+1) allows the element to stay in its current place, giving more options for randomization. 
+            setCurrentJoke(j)
         }
     }
 
-    const shuffleJokes = (array) => {
-        for(let i = array.length -1; i > 0; i--){
-            const j = Math.floor(Math.random() * (i + 1))//(i+1) allows the element to stay in its current place, giving more options for randomization. 
-            [array[i], array[j]] = [array[j], array[i]] //swap variables in one line in js. Eliminates need for placeholder
-        }
-        setJokes(array)
-    }
-
-    return(
+    return (
         <>
-            <Joke joke={ !jokes ? 'Loading...' : jokes[currentJoke].setup } />
-            <Punchline punchline={ jokes && jokes[currentJoke].punchline } />
-            <Button onPress={nextJoke} title="Next Joke" />
+            <Joke joke={!jokes ? 'Loading...' : jokes[currentJoke].setup} />
+            <Punchline punchline={jokes && jokes[currentJoke].punchline} />
+            <Button onPress={shuffleJokes} title="Next Joke" />
         </>
     )
 }
